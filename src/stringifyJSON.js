be16784add7966bac72f107f6b81bfd9761bc273
Naman Goel <naman34@gmail.com>
2014-01-23 10:26:25 +0530

    Implemented stringifyJSON completely. The test is erroring because an Array function is missing. My own tests are working flawlessly. My implementation is still not putting Dates in the right format though.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,35 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  var string = '';
+
+  (function stringify(obj){
+  	if(!!Array.isArray(obj)) {
+  		string += '[';
+  		obj.forEach(function(el, index, array){
+  			stringify(el);
+  			if(index !== array.length - 1){
+  				string += ', ';
+  			}
+  		});
+  		string += ']';
+  	} else if(typeof obj === 'object') {
+  		string += '{';
+  		var arr = [];
+  		for(key in obj){
+  			arr.push('"' + key + '"' + ':' + stringifyJSON(obj[key]));
+  		}
+  		string += arr.join(", ");
+  		string += '}';
+  	} else {
+  		if(typeof obj === 'number'){
+  			string += obj;
+  		} else {
+  			string += '"' + obj + '"';
+  		}
+  	}
+  })(obj);
+
+  return string;
+
 };
